c77373eb5e97b753312144872c33daf550064407
2inlee <kbs@kakao.com>
2024-04-01 16:30:27 +0900

    Spring Starter Initializing

---
+++ src/main/java/kr/co/introme/introme/IntromeApplication.java
@@ -0,0 +1,13 @@
+package kr.co.introme.introme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntromeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntromeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/introme/introme/IntromeApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.introme.introme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntromeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
