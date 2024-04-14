91017fff5745782be0144f4dccb1291b4b371823
2inlee <kbs@kakao.com>
2024-04-14 16:44:41 +0900

    dotenv properties config

--- src/main/java/kr/co/introme/introme/IntromeApplication.java
+++ src/main/java/kr/co/introme/introme/IntromeApplication.java
@@ -2,15 +2,22 @@ package kr.co.introme.introme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
-//@ComponentScan(basePackages = {"kr.co.introme.introme.domain"})
 public class IntromeApplication {
 
 	public static void main(String[] args) {
-//		System.out.println(IntromeApplication.class.getPackageName());
+		// .env 파일 로드
+		Dotenv dotenv = Dotenv.configure().directory("src/main/resources/.env").load();
+
+		// 시스템 프로퍼티로 환경 변수 설정
+		System.setProperty("DB_DRIVER", dotenv.get("DB_DRIVER"));
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
+		// 애플리케이션 실행
 		SpringApplication.run(IntromeApplication.class, args);
 	}
-
 }
