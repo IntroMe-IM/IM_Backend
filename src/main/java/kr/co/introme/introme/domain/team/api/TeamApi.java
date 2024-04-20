e5bd16a57bf2ba9b8a35b46688ad443759f043c8
vlqhel3440 <vlqhel3440@naver.com>
2024-04-20 17:46:28 +0900

    add Invite api & edit Team

--- src/main/java/kr/co/introme/introme/domain/team/api/TeamApi.java
+++ src/main/java/kr/co/introme/introme/domain/team/api/TeamApi.java
@@ -2,9 +2,7 @@ package kr.co.introme.introme.domain.team.api;
 
 import kr.co.introme.introme.domain.team.application.TeamBuildService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/team")
@@ -15,5 +13,11 @@ public class TeamApi {
     @PostMapping("/build")
     public void build() {
 
+    }
+
+
+    @GetMapping("/invite/@{code}")
+    public String invite(@PathVariable String code) {
+        return "null";
     }
 }
--- src/main/java/kr/co/introme/introme/domain/team/domain/Team.java
+++ src/main/java/kr/co/introme/introme/domain/team/domain/Team.java
@@ -19,25 +19,32 @@ public class Team {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    //팀 이름
     @Column(nullable = false)
     private String name;
 
-    @Column
+    //프로젝트 설명
+    @Column()
     private String description;
 
+    //프로젝트 이름
     @Column
     private String project;
 
+    //이미지 경로
     @Column
     private String image;
 
+    //팀 repo
     @Column
     private String url;
 
+    //생성 날짜
     @Column
     @Temporal(TemporalType.DATE)
     private Date createdDate;
 
+    //fk
     @Column
     private Long ownerId;
 
