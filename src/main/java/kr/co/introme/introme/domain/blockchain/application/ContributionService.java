2eb138b72d18ecf88354ee1d477bb6c88667334c
2inlee <kbs@kakao.com>
2024-05-02 15:49:12 +0900

    blockchain package build

---
+++ src/main/java/kr/co/introme/introme/domain/blockchain/application/ContributionService.java
@@ -0,0 +1,13 @@
+package kr.co.introme.introme.domain.blockchain.application;
+
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@NoArgsConstructor
+public class ContributionService {
+    private final ContributionRepository contributionRepository;
+
+
+}
+
