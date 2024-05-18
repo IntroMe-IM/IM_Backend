666fa9fd98ae5e2b9dabcb46be6df8b5da1edcd9
2inlee <kbs@kakao.com>
2024-05-18 21:20:30 +0900

    server swagger-ui error, edit SecurityConfig

--- src/main/java/kr/co/introme/introme/global/config/SecurityConfig.java
+++ src/main/java/kr/co/introme/introme/global/config/SecurityConfig.java
@@ -1,4 +1,5 @@
 package kr.co.introme.introme.global.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,18 +8,25 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(
-                        (auth) -> auth.requestMatchers("/").permitAll()
-                );
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(withDefaults());
 
         return http.build();
     }
+
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
