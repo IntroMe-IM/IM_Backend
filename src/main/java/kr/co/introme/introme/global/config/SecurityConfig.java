837e250e7a611ffcc4a44d8fbe3a64c28bea0158
2inlee <kbs@kakao.com>
2024-05-18 20:08:25 +0900

    Spring security FilterChain config

--- src/main/java/kr/co/introme/introme/global/config/SecurityConfig.java
+++ src/main/java/kr/co/introme/introme/global/config/SecurityConfig.java
@@ -1,11 +1,24 @@
 package kr.co.introme.introme.global.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(
+                        (auth) -> auth.requestMatchers("/").permitAll()
+                );
+
+        return http.build();
+    }
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
