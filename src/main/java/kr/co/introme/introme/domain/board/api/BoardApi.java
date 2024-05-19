8d175304972701c21953f65da8b34ed516213e31
vlqhel3440 <vlqhel3440@naver.com>
2024-05-19 21:27:56 +0900

    edit board entity add request mapping

--- src/main/java/kr/co/introme/introme/domain/board/api/BoardApi.java
+++ src/main/java/kr/co/introme/introme/domain/board/api/BoardApi.java
@@ -6,24 +6,22 @@ import kr.co.introme.introme.domain.board.dto.BoardPostRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/v1/board")
 @RequiredArgsConstructor
 public class BoardApi {
     private final BoardPostService boardPostService;
 
     @Operation(summary = "게시글 작성", description = "게시글 작성 정보를 저장합니다.")
-    @PostMapping("/board")
+    @PostMapping("/")
     public ResponseEntity<String> post(@RequestBody BoardPostRequest boardPostRequest){
         boardPostService.save(boardPostRequest);
         return ResponseEntity.ok("작성완료!");
     }
 
-    @GetMapping("/board/{board_id}")
+    @GetMapping("/{board_id}")
     public ResponseEntity<String> hit(@PathVariable Long board_id){
         String allHit = boardPostService.hit(board_id);
         return ResponseEntity.ok("총 조회수 = " + allHit + ".");
