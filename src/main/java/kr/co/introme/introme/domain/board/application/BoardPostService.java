07cb743de56f65f53948c101b81fe811dc3fb391
vlqhel3440 <vlqhel3440@naver.com>
2024-06-12 13:32:52 +0900

    feat : edit BoardPostService change Order by DESC

--- src/main/java/kr/co/introme/introme/domain/board/application/BoardPostService.java
+++ src/main/java/kr/co/introme/introme/domain/board/application/BoardPostService.java
@@ -12,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -42,7 +43,7 @@ public class BoardPostService {
     }
 
     public BoardPageResponse<BoardContentResponse> getBoards(int page, int size) {
-        Pageable pageable = PageRequest.of(page, size);
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Order.desc("createAt")));
         Page<Board> boardPage = boardRepository.findAll(pageable);
         List<BoardContentResponse> content = boardPage.getContent().stream()
                 .map(board -> BoardContentResponse.boardLists(
