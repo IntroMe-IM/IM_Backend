52e1016172b658567770c98621aad197c757298c
vlqhel3440 <vlqhel3440@naver.com>
2024-06-14 21:43:26 +0900

    refactor : Blockchain Domain

--- src/main/java/kr/co/introme/introme/domain/blockchain/domain/Blockchain.java
+++ src/main/java/kr/co/introme/introme/domain/blockchain/domain/Blockchain.java
@@ -23,36 +23,4 @@ public class Blockchain {
     @JoinColumn(name = "block_id")
     private List<Block> chain = new ArrayList<>();
 
-    public Blockchain() {
-        // Genesis block is created in addGenesisBlock method
-    }
-
-    public void addGenesisBlock(Block genesisBlock) {
-        chain.add(genesisBlock);
-    }
-
-    public Block getLatestBlock() {
-        return chain.get(chain.size() - 1);
-    }
-
-    public void addBlock(Block newBlock) {
-        newBlock = new Block(getLatestBlock().getHash(), newBlock.getMemberId(), newBlock.getContribution());
-        chain.add(newBlock);
-    }
-
-    public boolean isChainValid() {
-        for (int i = 1; i < chain.size(); i++) {
-            Block currentBlock = chain.get(i);
-            Block previousBlock = chain.get(i - 1);
-
-            if (!currentBlock.getHash().equals(currentBlock.calculateHash())) {
-                return false;
-            }
-
-            if (!currentBlock.getPreviousHash().equals(previousBlock.getHash())) {
-                return false;
-            }
-        }
-        return true;
-    }
 }
