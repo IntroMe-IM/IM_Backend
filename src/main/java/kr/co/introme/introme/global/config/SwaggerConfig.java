fab6e19c4ce59aa7620daacb6a2db1966abe739d
2inlee <kbs@kakao.com>
2024-06-15 00:23:14 +0900

    config: build.gradle and security config

--- src/main/java/kr/co/introme/introme/global/config/SwaggerConfig.java
+++ src/main/java/kr/co/introme/introme/global/config/SwaggerConfig.java
@@ -3,12 +3,7 @@ package kr.co.introme.introme.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.media.Content;
-import io.swagger.v3.oas.models.media.MediaType;
-import io.swagger.v3.oas.models.media.Schema;
-import io.swagger.v3.oas.models.parameters.RequestBody;
-import io.swagger.v3.oas.models.PathItem;
-import io.swagger.v3.oas.models.Operation;
+
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
