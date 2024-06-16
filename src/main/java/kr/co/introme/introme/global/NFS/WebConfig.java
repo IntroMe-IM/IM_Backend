acabaaba48ff41a77fd75f008548f302f5fcda5f
2inlee <kbs@kakao.com>
2024-06-17 01:11:04 +0900

    config: WebConfig for cors

--- src/main/java/kr/co/introme/introme/global/NFS/WebConfig.java
+++ src/main/java/kr/co/introme/introme/global/NFS/WebConfig.java
@@ -1,6 +1,7 @@
 package kr.co.introme.introme.global.NFS;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,5 +12,14 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/app/upload/**")
                 .addResourceLocations("file:///home/inlee/Desktop/workspace/introme/IM_Backend/local-upload-dir");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://introme.co.kr", "http://175.113.163.4:8081")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
